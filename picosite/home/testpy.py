d3d7b98c3bb47fba193e73b9f8b176070b901a2f
n0va000 <124531410+n0va000@users.noreply.github.com>
2023-05-21 01:16:18 +0100

    Update testpy.py

--- picosite/home/testpy.py
+++ picosite/home/testpy.py
@@ -5,7 +5,10 @@ gui.msgBox(gui.osk("type something"))
 gui.msgBox("Alright, try guess a number from 1 to 10!")
 number = random.randint(1,10)
 while 1:
-  picked = int(gui.osk("guess a number","0123456789"))
+  try:
+    picked = int(gui.osk("guess a number","0123456789"))
+  except:
+    picked = 0
   if number == picked:
     gui.msgBox("You win!!")
     break
