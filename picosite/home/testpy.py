8f8d5a5791a28c4bda789031eecc1404760726ff
n0va000 <124531410+n0va000@users.noreply.github.com>
2023-05-21 01:18:21 +0100

    Update testpy.py

--- picosite/home/testpy.py
+++ picosite/home/testpy.py
@@ -1,18 +1,18 @@
 import guilib as gui
 import random
-gui.msgBox("This is python!")
-gui.msgBox(gui.osk("type something"))
-gui.msgBox("Alright, try guess a number from 1 to 10!")
-number = random.randint(1,10)
-while 1:
-  try:
+try:
+  gui.msgBox("This is python!")
+  gui.msgBox(gui.osk("type something"))
+  gui.msgBox("Alright, try guess a number from 1 to 10!")
+  number = random.randint(1,10)
+  while 1:
     picked = int(gui.osk("guess a number","0123456789"))
-  except:
-    picked = 0
-  if number == picked:
-    gui.msgBox("You win!!")
-    break
-  elif picked > number:
-    gui.msgBox("target number is smaller")
-  elif number > picked:
-    gui.msgBox("target number is bigger")
+    if number == picked:
+      gui.msgBox("You win!!")
+      break
+    elif picked > number:
+      gui.msgBox("target number is smaller")
+    elif number > picked:
+      gui.msgBox("target number is bigger")
+Except:
+  gui.msgBox("Oh no an error occured in the demo")
